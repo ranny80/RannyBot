af7bb5aa9527cc5ac666afec0cd3ccc2e0c8e021
ranny80 <rannyvuong80@gmail.com>
2023-03-21 17:54:18 -0400

    Update mute.js

--- commands/mute.js
+++ commands/mute.js
@@ -3,11 +3,15 @@ const ms = require("ms");
 module.exports = {
     name: "mute",
     description: "Mutes a member from the server",
-    execute(message, args) {
-        if (message.member.hasPermission("MANAGE_ROLES")) {
+    execute(message, args)
+    {
+        if (message.member.hasPermission("MANAGE_ROLES"))
+        {
             const member = message.mentions.users.first();
-            if (member) {
-                if (!args[1]) {
+            if (member)
+            {
+                if (!args[1])
+                {
                     // Mutes the member
                     member.roles.add("Muted");
                     message.channel.send("User has been muted");
@@ -16,15 +20,20 @@ module.exports = {
                 member.roles.add("Muted");
                 message.channel.send(`User has been muted for ${ms(ms(args[1]))}`);
 
-                setTimeout(function() {
+                setTimeout(function()
+                {
                     // Unmutes the member with timed
                     member.roles.remove("Muted");
                     message.channel.send("User has been unmuted");
                 }, ms(args[1]));
-            } else {
+            } 
+            else
+            {
                 message.channel.send("Please specify a member.");
             }
-        } else {
+        }
+        else
+        {
             message.channel.send("You do not have permission to mute that members.");
         }
     }
