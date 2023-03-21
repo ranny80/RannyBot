1f35560bc42bafbe166837afdbf0807c26cb8fe1
ranny80 <rannyvuong80@gmail.com>
2023-03-21 17:52:04 -0400

    Update unmute.js

--- commands/unmute.js
+++ commands/unmute.js
@@ -4,14 +4,19 @@ module.exports = {
     execute(message, args) {
         if (message.member.hasPermission("MANAGE_ROLES")) {
             const member = message.mentions.users.first();
-            if (member) {
+            if (member)
+            {
                 // Unmutes the member
                 member.roles.remove("Muted");
                 message.channel.send("User has been unmuted");
-            } else {
+            }
+            else
+            {
                 message.channel.send("Please specify a member.");
             }
-        } else {
+        }
+        else 
+        {
             message.channel.send("You do not have permission to unmute that members.");
         }
     }
