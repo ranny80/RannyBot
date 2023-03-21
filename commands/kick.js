d88f26aa4e2ad2abc9bc59164ed9f7b9d2e72537
ranny80 <rannyvuong80@gmail.com>
2023-03-21 17:55:16 -0400

    Update kick.js

--- commands/kick.js
+++ commands/kick.js
@@ -1,19 +1,26 @@
 module.exports = {
     name: "kick",
     description: "Kicks a member out of the server",
-    execute(message, args) {
-        if (message.member.hasPermission("KICK_MEMBERS")) {
+    execute(message, args)
+    {
+        if (message.member.hasPermission("KICK_MEMBERS"))
+        {
             const member = message.mentions.users.first();
-            if (member) {
+            if (member)
+            {
                 const memberTarget = message.guild.members.cache.get(member.id);
                 
                 // Kicks the member
                 memberTarget.kick();
                 message.channel.send("User has been kicked");
-            } else {
+            }
+            else
+            {
                 message.channel.send("Please specify a member.");
             }
-        } else {
+        }
+        else
+        {
             message.channel.send("You do not have permission to kick that members.");
         }
     }
